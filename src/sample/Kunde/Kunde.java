cbcf1c75fe1ca41110aee61b517cefbf6bfa3a4a
Andreas <s333770@oslomet.no>
2019-09-10 12:17:39 +0200

    Done some more work on basic syntaks

---
+++ src/sample/Kunde/Kunde.java
@@ -0,0 +1,81 @@
+package sample.Kunde;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Kunde {
+    public static ObservableList<Kunde> kundeForsikring= FXCollections.observableArrayList();
+
+    private SimpleStringProperty fornavn;
+    private SimpleStringProperty etternavn;
+    private SimpleStringProperty email;
+    private SimpleStringProperty pris;
+
+    public static ObservableList<Kunde> getKundeForsikring() {
+        return kundeForsikring;
+    }
+
+    public static void setKundeForsikring(ObservableList<Kunde> kundeForsikring) {
+        Kunde.kundeForsikring = kundeForsikring;
+    }
+
+    public String getFornavn() {
+        return fornavn.get();
+    }
+
+    public SimpleStringProperty fornavnProperty() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn.set(fornavn);
+    }
+
+    public String getEtternavn() {
+        return etternavn.get();
+    }
+
+    public SimpleStringProperty etternavnProperty() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn.set(etternavn);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public String getPris() {
+        return pris.get();
+    }
+
+    public SimpleStringProperty prisProperty() {
+        return pris;
+    }
+
+    public void setPris(String pris) {
+        this.pris.set(pris);
+    }
+
+    public Kunde(String fornavn, String etternavn, String email, String pris) {
+        this.fornavn = new SimpleStringProperty(fornavn);
+        this.etternavn = new SimpleStringProperty(etternavn);
+        this.email = new SimpleStringProperty(email);
+        this.pris = new SimpleStringProperty(pris);
+    }
+    @Override
+    public String toString(){
+        return "Navn: "+fornavn+" "+etternavn+"Email: "+email+"Pris: "+pris;
+    }
+}
--- src/sample/kjop/KjopController.java
+++ src/sample/kjop/KjopController.java
@@ -1,4 +1,73 @@
 package sample.kjop;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import sample.Kunde.Kunde;
+
+import java.util.InputMismatchException;
+
+import static sample.Kunde.Kunde.kundeForsikring;
+
 public class KjopController {
+
+    @FXML
+    private TextField txtFornavn;
+
+    @FXML
+    private TextField txtEtternavn;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtPris;
+
+
+    @FXML
+        void bekreftKjop(ActionEvent event) {
+            kundeForsikring.add(new Kunde(txtFornavn.getText(),txtEtternavn.getText(),txtEmail.getText(),"1000"));
+        }
+
+        @FXML
+        void fritidKjop(ActionEvent event) {
+
+        }
+
+        @FXML
+        void kjopBat(ActionEvent event) {
+
+        }
+
+        @FXML
+        void kjopInnbo(ActionEvent event) {
+
+        }
+
+        @FXML
+        void reiseKjop(ActionEvent event) {
+
+        }
+    Alert informasjonboks = new Alert(Alert.AlertType.INFORMATION); // Lager en alarmboks
+
+        public void sjekkOmAlleFeltErFyltUt() throws InputMismatchException {
+            try {
+                if (txtFornavn.getText() == null || txtEtternavn.getText() == null || txtEmail.getText() == null) {
+                    throw new InputMismatchException("Alle feltene er ikke fylt ut ");
+                }
+            }
+            catch(InputMismatchException e){
+                System.err.println(e.getMessage());
+                informasjonboks.setTitle("Feil i en av inputfeltene");
+                informasjonboks.setContentText(e.getMessage());
+                informasjonboks.show();
+            }
+        }
+
 }
+
+
+
+
+
